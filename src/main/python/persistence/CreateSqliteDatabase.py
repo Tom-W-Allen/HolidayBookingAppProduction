e06d5ae0a9cec4b391b88a26069b779e65f0eb8e
Tom Allen <tomwa1@live.co.uk>
2024-12-11 13:21:23 +0000

    Corrected directory placement

--- src/main/python/persistence/CreateSqliteDatabase.py
+++ src/main/python/persistence/CreateSqliteDatabase.py
@@ -4,7 +4,7 @@ import os
 
 def set_up_sqlite_database():
     current_file_path = os.path.dirname(__file__).split("\\")
-    script_file_path = ("\\".join(current_file_path[:len(current_file_path) - 3])) + "\\"
+    script_file_path = ("\\".join(current_file_path[:len(current_file_path) - 4])) + "\\"
 
     with open(f"{script_file_path}Holiday App Database Creation Script.sql", "r") as file:
         script = file.read()
